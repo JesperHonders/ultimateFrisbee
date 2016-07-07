bd1b652100fac620b532c64e8d46c91617947b5b
JesperHonders <jesper.honders@hva.nl>
2016-07-07 12:06:18 +0200

    Removed add account code due crashes

--- server/main.js
+++ server/main.js
@@ -3,8 +3,4 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // runs polling of api
   Meteor.call("updateResults")
-  Accounts.createUser({
-      email: "admin@windmill.com",
-      password: "test1234"
-  });
 });
