69099119da9b6ec63519b870f528c038240d54d4
JesperHonders <jesper.honders@hva.nl>
2016-07-07 11:59:35 +0200

    Always has an admin account now

--- server/main.js
+++ server/main.js
@@ -3,4 +3,8 @@ import { Meteor } from 'meteor/meteor';
 Meteor.startup(() => {
   // runs polling of api
   Meteor.call("updateResults")
+  Accounts.createUser({
+      email: "admin@windmill.com",
+      password: "test1234"
+  });
 });
